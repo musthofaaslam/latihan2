599e009c190d124f77747b991c4f95cc6dc20b5a
Ahmad Aslam <musthofaaslam29@gmail.com>
2024-11-07 16:46:10 +0700

    edit

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>halo</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perferendis nam nesciunt, pariatur impedit omnis officia incidunt veritatis architecto deleniti eveniet, maxime libero unde dolore? Dolorem optio doloremque possimus praesentium.</p>
 </body>
 </html>
